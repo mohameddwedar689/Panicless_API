864ce9561ca184e01d8c921d64d2d48d924dccbe
werdani <ammaryasser554zz@gmail.com>
2023-04-21 04:55:26 +0200

    [update] settings add allow hosts

--- panicless/settings.py
+++ panicless/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-_-@8gzh)!)hb2^ttdv)@y#ww-yb+a1fp8ii*2isah!28u@@2pb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

fba1a84c18b03a1b4b8134ed610cbd4aebef32f2
werdani <ammaryasser554zz@gmail.com>
2023-04-23 19:00:27 +0200

    [update] settings add whitenois

--- panicless/settings.py
+++ panicless/settings.py
@@ -13,6 +13,11 @@ from datetime import timedelta
 from pathlib import Path
 import os
 
+
+# directory = r'C:\Users\DELL\Desktop\Panicless_API\static'
+#
+# if not os.path.exists(directory):
+#     os.makedirs(directory)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 AUTH_USER_MODEL = "account.User"
@@ -50,6 +55,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -155,8 +161,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
